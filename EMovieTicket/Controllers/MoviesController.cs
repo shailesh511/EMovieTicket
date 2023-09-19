36f65d5434a6bb812f64531fa8d9318d422033d9
Sailesh <shaileshtripathi511@gmail.com>
2023-09-19 15:09:12 +0530

    Added Nav bar and updated home page route

--- EMovieTicket/Controllers/MoviesController.cs
+++ EMovieTicket/Controllers/MoviesController.cs
@@ -1,6 +1,7 @@
 ﻿using EMovieTicket.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace EMovieTicket.Controllers
@@ -14,9 +15,9 @@ namespace EMovieTicket.Controllers
             _appdbContext = appdbContext;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index()                
         {
-            var moviesList = await _appdbContext.Movies.Include(n=>n.Cinema).ToListAsync();
+            var moviesList = await _appdbContext.Movies.Include(n=>n.Cinema).OrderBy(n=>n.Name).ToListAsync();
             return View(moviesList);
         }
     }
